65d98db83ab894bead80af3e89322117cc5bf07a
AlexKr <alexkrokhmall@gmail.com>
2016-06-13 21:32:26 +0300

    First commit of home task 2 (variand #1)

---
+++ Hometask2/2-1/Hometask2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            for (int i = 1; i < 101; i++)
+            {
+                                
+                if (i % 3 != 0)
+                {
+                    if (i % 5 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                }
+                else if (i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+            }
+                       
+            Console.ReadKey();
+        }
+    }
+}
